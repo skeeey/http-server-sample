355732336c1b0fa5ff21b5dc0d0b68b832895ce8
liuwei <liuweixa@redhat.com>
2021-02-07 17:32:14 +0800

    http-server sample

---
+++ http-server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+const ServerPortKey = "SERVER_PORT"
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "hello\n")
+}
+
+func main() {
+	http.HandleFunc("/hello", hello)
+
+	serverPort, existing := os.LookupEnv(ServerPortKey)
+	if !existing {
+		serverPort = "8000"
+	}
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", serverPort), nil))
+}
